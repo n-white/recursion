017dbb18596858c09789ec24360dd53093e52e18
n-white <neil.r.whitehead@gmail.com>
2016-05-05 00:45:14 +0100

    "Starting over with a simpler version and using map to pass in elements of an array"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,34 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  
-
-	arrayFunc = function(item) {
-
-  		return "[" + item.toString() + "]"
-  		
-	};
-	
-	objFunc = function(item) {
-		var result = "{"
-		for(key in item) {
-			result += '"' + key.toString() + '"' + ":" + item[key].toString();
-		};
-		return result + "}";
-	}
-	
-	stringFunc = function(item) {
-		return '"' + item + '"';
-	}
-	
-  	if(Array.isArray(obj)) {
-	  	return arrayFunc(obj);
-  	} else if (typeof obj == 'object') {
-  		return objFunc(obj);
-  	} else if (typeof obj == 'string') {
-  		return strFunc(obj)
-  	} else {
-  		return obj.toString();
-  	}
+each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for(var i = 0; i < collection.length; i ++) {
+        iterator(collection[i], i, collection);
+      };
+    } else {  
+      for(var key in collection) {
+        iterator(collection[key], key, collection);
+      };
+    };
 };
+
+map = function(collection, iterator) {
+    // map() is a useful primitive iteration function that works a lot
+    // like each(), but in addition to running the operation on all
+    // the members, it also maintains an array of results.
+    var newArray = [];
+    each(collection, function(item) {
+      newArray.push(iterator(item));
+    });
+    return newArray;
+  };
+
+
+var stringifyJSON = function(obj) {
+	if(typeof obj == undefined) {
+		return null;
+	}
+	if(typeof obj == 'string') {
+		return '"' + obj + '"';
+	}
+	if(typeof obj == 'boolean') {
+		return obj;
+	}
+	if(typeof obj == 'number') {
+		return "'" + obj "'";
+	}
+	if(Array.isArray(obj)) {
+		return "[" + map(obj, function(item) {return stringifyJSON(item)} ).join(",") + "]"
+	}
+	
+}
