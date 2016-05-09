46af6c5789fe9ca3bc57a03cadf635d1c244e710
n-white <neil.r.whitehead@gmail.com>
2016-05-09 20:38:29 +0100

    "Pasted in my contains method code from my underbar.js file"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -17,15 +17,22 @@ var getElementsByClassName = function(className) {
     if(contains(element.classList, className)) {
       elementList.push(element.classList)
     }
-    
-    
-
-
+        
   }
   helperFunction(document.body);
   return elementList;
-
-
-
 };
+
+
+
+contains = function(collection, target) {
+    // TIP: Many iteration problems can be most easily expressed in
+    // terms of reduce(). Here's a freebie to demonstrate!
+    return _.reduce(collection, function(wasFound, item) {
+      if (wasFound) {
+        return true;
+      }
+      return item === target;
+    }, false);
+  };
 
