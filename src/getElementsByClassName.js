1e018a64a4c76cf4f5c7f7a832c7a5fe815b7d56
n-white <neil.r.whitehead@gmail.com>
2016-05-09 19:39:21 +0100

    "Initial outline using an empty array and a helper function to append elements to the array"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+
+var getElementsByClassName = function(className) {
   // your code here
+
+  var elementList = [];
+  function helperFunction(element) {
+
+    //Setup helper function to call traverse the DOM and collect each element with className
+    //Helper function will call itself recursively
+
+
+  }
+  helperFunction(document.body);
+  return elementList;
+
+
+
 };
+
+
